244ddc3d103a3f448012f2e2b8a9a57c26fcc3b6
PetrMakeev <109295765+PetrMakeev@users.noreply.github.com>
2022-12-12 21:06:04 +0500

    Архивирование с сохранением путей

--- ZipFolders.py
+++ ZipFolders.py
@@ -1,10 +1,11 @@
 import zipfile
+from zipfile import allowZip64
 import os
 
 def ZipFiles():
     z = zipfile.ZipFile('d:\\!test\\arc.zip', 'w')        # Создание нового архива
     for root, dirs, files in os.walk('d:\\Backup\\arc'): # Список всех файлов и папок в директории folder
         for file in files:
-            z.write(os.path.join(root,file))         # Создание относительных путей и запись файлов в архив
+            z.write(os.path.join(root,file), allowZip64 )         # Создание относительных путей и запись файлов в архив
 
     z.close()
